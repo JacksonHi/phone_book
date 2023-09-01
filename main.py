a1470a167c28ebc6f9330242c990f0ff1087dafc
Jackson <HiEvgenij@yandex.ru>
2023-09-01 21:06:36 +0300

    доработал поиск

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG, encoding='utf-8')
+logging.basicConfig(level=logging.INFO, encoding='utf-8')
 
 
 DI = {
@@ -17,17 +17,22 @@ DI = {
 
 def print_phone_book(phone_book: list) -> None:
     """ Вывод записей построчно """
-    for i in phone_book:
+    logging.debug(phone_book)
+    for item in phone_book:
         user = []
-        for key, value in i.items():
-            user.append(f'{DI[key]}: {value}')
+        for key, value in item.items():
+            if key == 'id':
+                user.append(f'id: {item["id"]}')
+            else:
+                user.append(f'{DI[key]}: {value}')
         print(', '.join(user))
 
 
-def creating_record():
+def creating_record(phone_book, last_id):
     """ Добавление новой записи пользователя """
     print('Создание новой записи')
     new_user = {key: input(f'{value}: ') for key, value in DI.items()}
+    new_user['id'] = str(last_id + 1)
     phone_book.append(new_user)
     print('Запись добавлена')
 
@@ -37,15 +42,43 @@ def editing_an_entry():
     pass
 
 
-def search_record(**kwargs) -> None:
+def search_record(phone_book: list, **kwargs) -> list:
     """ Поиск записей """
-    
-    search_result = [
-        value
-        for value in phone_book
-        if value['firstname'] == kwargs[name]
-    ]
-    print_phone_book(search_result)
+    return filter(
+        lambda data: all([data[key] == value for key, value in kwargs.items()]),
+        phone_book
+    )
+
+
+def entering_search_parameters() -> dict:
+    """ Ввод параметров поиска """
+    params = {}    # словарь с параметрами
+    print('Выбор характеристики по которой будет произведен поиск:\n'
+          '1 - Имя\n'
+          '2 - Фамилия\n'
+          '3 - Отчество\n'
+          '4 - Название организации\n'
+          '5 - Телефон рабочий\n'
+          '6 - Телефон личный\n'
+          '0 - Поиск'
+          )
+    # вспомогательный список для выбора характеристики
+    menu = ['Поиск', 'firstname', 'lastname', 'patronymic', 'organization_name',
+            'work_phone', 'personal_phone']
+    while True:
+        number = input('Номер параметра: ')
+        try:
+            key = menu[int(number)]
+            if number == '0':
+                break    # все параметры выбраны, начало поиска
+            elif key in DI:
+                value = input('Значение: ')
+                params[key] = value    # добавляются параметры для поиска
+        except IndexError:
+            print('Не верный номер параметра')
+        except ValueError:
+            print('Это не похоже на номер параметра')
+    return params
 
 
 if __name__ == '__main__':
@@ -71,31 +104,23 @@ if __name__ == '__main__':
         elif action == '1':
             print_phone_book(phone_book)
         elif action == '2':
-            creating_record()
+            creating_record(phone_book, int(phone_book[-1]['id']))
         elif action == '3':
-            editing_an_entry()
+            print(
+                '1 - Ввести id контакта для редактирования'
+                '2 - Перейти к поиску контакта')
+            number = input('Номер параметра: ')
+            if number == 1:
+                id_user = input('id: ')
+                for user in phone_book:
+                    
+            if number == 2:
+                params = entering_search_parameters()    # выбор параметров
+                user = search_record(phone_book, **params)    # поиск
+                print_phone_book(user)
         elif action == '4':
-            params = {}    # словарь с параметрами
-            print('Выбор характеристики по которой будет произведен поиск:\n'
-                  '1 - Имя\n'
-                  '2 - Фамилия\n'
-                  '3 - Отчество\n'
-                  '4 - Название организации\n'
-                  '5 - Телефон рабочий\n'
-                  '6 - Телефон личный\n'
-                  '0 - Поиск'
-                  )
-            # вспомогательный список для выбора характеристики
-            menu = ['Поиск', 'firstname', 'lastname', 'patronymic', 'organization_name',
-                    'work_phone', 'personal_phone']
-            while True:
-                number = input('Номер параметра: ')
-                key = menu[int(number)]
-                if number == '0':
-                    break    # все параметры выбраны, начало поиска
-                elif key in DI:
-                    value = input('Значение: ')
-                    params[key] = value    # добавляются параметры для поиска
-            search_record(**params)
+            params = entering_search_parameters()    # выбор параметров
+            list_found_items = search_record(phone_book, **params)    # поиск
+            print_phone_book(list_found_items)
         else:
             print('операция не определена')
